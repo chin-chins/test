fe2d8bf3fd303e5c73630958cc5149886a21ac86
chin-chins <chimdinman0612@gmail.com>
2025-11-13 19:03:03 -0800

    Adding a profile card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import drRatio from './assets/dr_ratio.jpg'
 import './App.css'
 
 function App() {
@@ -28,6 +29,13 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div className="new-card">
+        <img src={drRatio} alt="dr ratio" className="profilepic"></img>
+        <h2>chi</h2>
+        <p className="intro">hello my name is chi i like popcorn and da pitt</p>
+        <p className="email">my email: chimdinman0612@gmail.com</p>
+      </div>
     </>
   )
 }
